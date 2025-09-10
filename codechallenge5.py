0dcb2d34b4be4105c575169ebc1cc14a130cb485
KentRovic <kentrovicloveria759@gmail.com>
2025-09-10 20:32:11 +0800

    initiate

---
+++ codechallenge5.py
@@ -0,0 +1,27 @@
+
+imp1 = input ("Hello welcome to Manga Suggestor What is your Name? ->")
+print(f"Hello {imp1} what genre do you like?")
+
+Gen = input ("enter choice G1/G2/G3: 1=Comedy//2=Action//3=Advernture:")
+Dec = input ("enter your choice D1/D2/D3: 1=Old_19s//2=PreModern_2010s//3=Modern_2020s:")
+
+if Gen == "1" and Dec == "1":
+ print("90s Comedy Recommendation Are: \n 'Ranma ½', 'Great Teacher Onizuka', 'Slam Dunk' \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "1" and Dec == "2":
+ print("2010s Comedy Recommendation Are \n 'Gintama', 'Nichijou', 'Daily Lives of High School Boys' \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "1" and Dec == "3":
+ print("2020s Comedy Recommendation Are \n 'Spy x Family', 'Kaguya-sama: Love is War', 'Komi Can't Communicate' \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "2" and Dec == "1":
+ print("90s Action Recommendation are \n Yu Yu Hakusho, Rurouni Kenshin, Berserk \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "2" and Dec == "2":
+ print("2010s Action Recommendation are \n Attack on Titan, Tokyo Ghoul, One Punch Man \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "2" and Dec == "3":
+ print("2020s Action Recommendation are \n Jujutsu Kaisen, Chainsaw Man, Hell’s Paradise \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "3" and Dec == "1":
+ print("90s Adventure Recommendation are \n Hunter x Hunter, One Piece, Magic Knight Rayearth \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "3" and D == "2":
+ print("2010s Adventure Recommendation are \n Made in Abyss, The Promised Neverland, Magi: The Labyrinth of Magic \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+elif Gen == "3" and D3 == "3":
+ print("2020s Adventure Recommendation are \n Frieren: Beyond Journey’s End, Ranking of Kings, Kaiju No. 8 \n These are the results of the known idea and research on manwha i know and searched more would be added {imp1} enjoy this reccomendations")
+else:
+ print("No Data Found There Might be a mistype or no info found sorry")
