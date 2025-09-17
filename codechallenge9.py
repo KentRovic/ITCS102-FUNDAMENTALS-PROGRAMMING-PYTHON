005b35fae9631bcd8e2bb20719fb2cd378c4b7c7
KentRovic <kentrovicloveria759@gmail.com>
2025-09-17 20:54:55 +0800

    initiate

---
+++ codechallenge9.py
@@ -0,0 +1,12 @@
+#Timer Simulator
+
+imp1 = int(input("What number is to use for countdown: "))
+
+print("\nCountdown starts:")
+
+
+for i in range(imp1, 0, -1):
+    print(i)
+
+
+print("Times Up. Liftoff!")
