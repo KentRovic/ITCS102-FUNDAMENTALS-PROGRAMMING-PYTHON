92d6fe077d1f35d053f255c3d7978e8b63a45077
KentRovic <kentrovicloveria759@gmail.com>
2025-09-17 20:56:18 +0800

    Remove all committed files

--- codechallenge6.py
+++
@@ -1,5 +0,0 @@
-val = 1
-inp1= eval(input("Type Number Here"))
-for y in range (inp1, 0, -1):
-    val *= y
-    print("The Factorial number you input is", val)
--- codechallenge8.py
+++
@@ -1,6 +0,0 @@
-imp1 = eval(input("input a number to view its multiplication numbers: "))
-
-
-for z in range(1, 11):
-
-    print(f"{imp1} x {z} = {imp1 * z}")
